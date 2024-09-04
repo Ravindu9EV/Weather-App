cf048ccb941247e715e5f2aed34469d0073a4dfc
Yasantha Ravindu <99851400+Ravindu9EV@users.noreply.github.com>
2024-09-05 05:22:31 +0530

    added geolocation

--- assets/app.js
+++ assets/app.js
@@ -49,6 +49,10 @@ faren.addEventListener("click", () => {
   //   console.log(celsius.childNodes[3].src.contains);
 });
 
+// ---------------get Input-----------
+
+//
+
 celsius.addEventListener("click", () => {
   let temp = celsius.childNodes[3].src;
   let tempTxt = celsius.childNodes[1].textContent;
@@ -73,9 +77,21 @@ celsius.addEventListener("click", () => {
 // ---------------------Add Card ------------------------------------
 
 let details = [];
+
+let latitudeG, longitudeG, tempF, windMph, fLikeF, dewPF, pressureM;
+let country, region, cityN, time;
+let map;
+document.getElementById("btn-search").addEventListener("click", () => {
+  search();
+  console.log(document.getElementById("txt-search").value);
+});
+document.getElementById("txt-search").addEventListener("click", () => {
+  document.getElementById("txt-search").value = "";
+  window.onload;
+});
 function search() {
-  let location = document.getElementById("txt-search").value;
   document.getElementById("btn-search").addEventListener("click", () => {
+    var location = document.getElementById("txt-search").value;
     fetch(
       `http://api.weatherapi.com/v1/current.json?key=6b9fad01b91c43c4b36102328242608&q=${location}&aqi=no`
     )
@@ -84,37 +100,121 @@ function search() {
   });
   details.forEach((d) => {
     console.log(d);
-    var temC = d.current.temp_c;
+    var tempC = d.current.temp_c;
+    tempF = d.current.temp_f;
     var fLike = d.current.feelslike_c;
-
+    fLikeF = d.current.feelslike_f;
     var humidity = d.current.humidity;
     var textWCond = d.current.condition.text;
     var pressure = d.current.pressure_in;
+    pressureM = d.current.pressure_mb;
     var wUrl = d.current.condition.icon;
     var dewP = d.current.dewpoint_c;
-
+    dewPF = d.current.dewpoint_f;
     var windDir = d.current.wind_dir;
     var windKph = d.current.wind_kph;
+    windMph = d.current.wind_mph;
     console.log(d.current.wind_mph);
-    if (
-      (celsius.childNodes[1].textContent.startsWith("f") &&
-        windMeasurment.startsWith("k")) ^
-      (celsius.childNodes[1].textContent.startsWith("f") &&
-        windMeasurment.startsWith("m"))
-    ) {
-      temC = d.current.temp_f;
-      fLike = cd.current.feelslike_f;
-      dewP = d.current.dewpoint_f;
-      if (windMeasurment.startsWith("m")) {
-        windKph = d.current.wind_mph;
-      }
-      if (pressMeasurement.startsWith("i")) {
-        pressure = d.current.pressure_in;
-      }
+
+    latitudeG = d.location.lat;
+    longitudeG = d.location.lon;
+    var lattt = d.location.lat;
+    var lontt = d.location.lon;
+    // country=d.location.Regi;
+    cityN = d.location.name;
+    region = d.location.region;
+    country = d.location.country;
+
+    var timeZone = d.location.tz_id;
+    console.log(cityN);
+
+    // --------get longitude and latitude--------
+    console.log(d.location.lon);
+    document;
+
+    if (document.querySelector(".wImage")) {
+      document
+        .getElementById("w-row")
+        .removeChild(document.querySelector(".wImage"));
     }
 
+    if (document.querySelector(".wCard")) {
+      document
+        .getElementById("w-row")
+        .removeChild(document.querySelector(".wCard"));
+    }
+
+    let newCard = document.createElement("div");
+    newCard.classList.add("wCard");
+
+    newCard.innerHTML = `
+    
+    <h4 id="locDHead">Weather details</h4>
+              <h6 id="feelsL">Humidity</h6>
+              <div class="midRow">
+                <img id="wCond" src="images/pCloudy.png" alt="">
+                <p id="cTemp">Temperature</p>
+                <p id="txtCond">cloud</p>
+              </div>
+              <div class="bRow">
+                <div class="dSub">
+                  <div class="headSec">
+                    <p  class="dHead">Wind</p>
+                  <select id="windSelect">
+                    <option value="kph" selected>kph</option>
+                    <option value="mph">mph</option>
+                  </select>
+                  </div>
+                <p id="windD" class="dBody">7878.12&</p>
+                </div>
+                <div class="dSub">
+                  <div class="headSec">
+                    <p  class="dHead">Pressure</p>
+                  <select id="presSelect">
+                    <option value="in" selected>in</option>
+                    <option value="mb">mb</option>
+                  </select>
+                  </div>
+                <p id="pressD" class="dBody">7878.12</p>
+                </div>
+                <div class="dSub">
+                  <p  class="dHead">Humidity</p>
+                <p id="humidD" class="dBody">7878.12</p>
+                </div>
+                <div class="dSub">
+                  <div class="headSec">
+                    <p   id="dew" class="dHead">Dew Point</p>
+                  <select id="dewSelect">
+                    <option value="celsius" selected>&#8451;</option>
+                    <option value="farenhite">&#8457;</option>
+                  </select>
+                  </div>
+                <p id="dewD" class="dBody">7878.12</p>
+                </div>
+              </div>
+    
+    
+    `;
+    document.getElementById("w-row").appendChild(newCard);
+
+    let newMap = document.createElement("div");
+    newMap.id = "map";
+    newMap.classList.add("wImage");
+    newMap.style.height = "100%";
+    document.getElementById("w-row").appendChild(newMap);
+
+    map = L.map("map").setView([51.505, -0.09], 13);
+
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      maxZoom: 19,
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(map);
+
+    // ----------------------------
+
     setDetails(
-      temC,
+      tempC,
       textWCond,
       wUrl,
       windKph,
@@ -122,7 +222,9 @@ function search() {
       pressure,
       fLike,
       humidity,
-      windDir
+      windDir,
+      lattt,
+      lontt
     );
   });
 }
@@ -136,10 +238,38 @@ function setDetails(
   pressure,
   feelsLike,
   humid,
-  windDir
+  windDir,
+  lattt,
+  lontt
 ) {
   document.getElementById("wCond").src = url;
-  document.getElementById("cTemp").innerText = temp;
+
+  document.getElementById("cTemp").innerText = temp + " \u00B0C";
+  document.getElementById("feelsL").innerText = feelsLike + " \u00B0C";
+  document.getElementById("dewD").innerText = dew + " \u00B0C";
+
+  celsius.addEventListener("click", () => {
+    if (celsius.childNodes[1].textContent.startsWith("f")) {
+      document.getElementById("cTemp").innerText = tempF + " \u00B0F";
+      document.getElementById("feelsL").innerText = fLikeF + " \u00B0F";
+      document.getElementById("dewD").innerText = dewPF + " \u00B0F";
+    } else {
+      document.getElementById("cTemp").innerText = temp + " \u00B0C";
+      document.getElementById("feelsL").innerText = feelsLike + " \u00B0C";
+      document.getElementById("dewD").innerText = dew + " \u00B0C";
+    }
+  });
+  faren.addEventListener("click", () => {
+    if (celsius.childNodes[1].textContent.startsWith("f")) {
+      document.getElementById("cTemp").innerText = tempF + " \u00B0F";
+      document.getElementById("feelsL").innerText = fLikeF + " \u00B0F";
+      document.getElementById("dewD").innerText = dewPF + " \u00B0F";
+    } else {
+      document.getElementById("cTemp").innerText = temp + " \u00B0C";
+      document.getElementById("feelsL").innerText = feelsLike + " \u00B0C";
+      document.getElementById("dewD").innerText = dew + " \u00B0C";
+    }
+  });
 
   // -------select Measurement--------
   const choosedVal = document.getElementById("windSelect");
@@ -147,28 +277,107 @@ function setDetails(
   choosedVal.addEventListener("click", () => {
     windMeasurment = document.getElementById("windSelect").value;
     if (windMeasurment.startsWith("m")) {
-      document.getElementById("windD").innerText = wind + " mph";
+      document.getElementById("windD").innerText = windMph + " mph";
     } else {
       document.getElementById("windD").innerText = wind + " kph";
     }
   });
 
+  // ------------Pressure selection-----------
   const choosedPres = document.getElementById("presSelect");
   var pressMeasurement = choosedPres.value;
   choosedPres.addEventListener("click", () => {
     pressMeasurement = document.getElementById("presSelect").value;
     if (pressMeasurement.startsWith("m")) {
-      document.getElementById("pressD").innerText = pressure + " mb";
+      document.getElementById("pressD").innerText = pressureM + " mb";
     } else {
       document.getElementById("pressD").innerText = pressure + " in";
     }
   });
   document.getElementById("windD").innerText = wind + " kph";
-  document.getElementById("pressD").innerText = pressure + " mb";
-  document.getElementById("dewD").innerText = dew;
-  document.getElementById("feelsL").innerText = feelsLike;
+
+  // ---------------------- Dew Point Selection---------
+
+  const choosedDew = document.getElementById("dewSelect");
+  var dewMeasurement = choosedDew.value;
+
+  choosedDew.addEventListener("click", () => {
+    dewMeasurement = choosedDew.value;
+    console.log(dewMeasurement);
+
+    if (dewMeasurement.startsWith("f")) {
+      document.getElementById("dewD").innerText = dewPF + " \u00B0F";
+    } else {
+      document.getElementById("dewD").innerText = dew + " \u00B0C";
+    }
+  });
+
+  //---------------------------------------------------
+
+  if (pressMeasurement.startsWith("i")) {
+    document.getElementById("pressD").innerText = pressure + " in";
+  } else {
+    document.getElementById("pressD").innerText = pressureM + " mb";
+  }
+
   document.getElementById("humidD").innerText = humid;
   document.getElementById("txtCond").innerText = txtCondition;
+
+  // ----------set card head details-----------
+
+  const title = document.getElementById("locDHead");
+  title.innerText = cityN + ", " + region + ", " + country;
+
+  longitudeG = lontt;
+  latitudeG = lattt;
+  success();
 }
 
 // ---------------------------------------
+
+//----------------Geo location-------------------
+// -----------used google maps example---------
+
+window.onload = function () {
+  map = L.map("map").setView([51.505, -0.09], 13);
+
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+};
+
+navigator.geolocation.watchPosition(success, error);
+
+let marker, circle, zoomed;
+
+function success(position) {
+  const latt = position.coords.latitude;
+  const lngt = position.coords.longitude;
+  const accuracy = position.coords.accuracy;
+  if (marker) {
+    map.removeLayer(marker);
+    map.removeLayer(circle);
+  }
+  if (latitudeG != null && longitudeG != null) {
+    marker = L.marker([latitudeG, longitudeG]).addTo(map);
+    circle = L.circle([latitudeG, longitudeG], { radius: accuracy }).addTo(map);
+    map.setView([latitudeG, longitudeG]);
+  } else {
+    marker = L.marker([latt, lngt]).addTo(map);
+    circle = L.circle([latt, lngt], { radius: accuracy }).addTo(map);
+  }
+
+  if (!zoomed) {
+    zoomed = map.fitBounds(circle.getBounds());
+  }
+}
+
+function error(err) {
+  if (err.code === 1) {
+    alert("Please allow geolocation access");
+  } else {
+    alert("Cannot get current location");
+  }
+}
